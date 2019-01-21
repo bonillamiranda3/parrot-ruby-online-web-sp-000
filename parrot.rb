3cf4e0551f13ee762383d9a8af5083e13eba510c
bonillamiranda3 <bonillamiranda3@gmail.com>
2019-01-21 02:53:16 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,9 +1,9 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 def parrot(phrase = "Squawk!")
-  puts "Squawk!"
-  return "Squawk!"
-  "Squawk!"
+  puts phrase
+  return phrase
+
 end
 
-parrot
+
